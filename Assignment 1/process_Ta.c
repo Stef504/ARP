cf56dcd784225fde20d2514c054f389e0910c3d7
Francesco <f.sisani2002@gmail.com>
2025-11-15 16:20:08 +0100

    Implemented coordinate generation and pipe writing in process_Ob and process_Ta; - Add Parameter_File.txt

--- Assignment 1/process_Ta.c
+++ Assignment 1/process_Ta.c
@@ -21,8 +21,29 @@ int main(int argc, char *argv[])
     int fdTa = atoi(argv[1]);
     char buffer[100];
     
-    //generate random coordinates 
-    //write it to pipe
-    
+    FILE* file = fopen("Parameter_File.txt", "r"); 
+    char line[256]; 
+    int i = 0; 
+    while (fgets(line, sizeof(line), file)) { 
+        i++; 
+        if (i == 1){
+            char* string1[] = delimit(line, "_");
+        }
+        if (i == 2){
+            char* string2[] = delimit(line, "_");
+        }
+
+    } 
+    fclose(file);
+
+    while(1){
+        //write it to pipe
+        x_coord = rand() % (0, string1[2]);
+        y_coord = rand() % (0, string2[2]);
+
+        sprintf(buffer, "%d,%d", x_coord, y_coord);
+        write(fdTa, buffer, strlen(buffer)+1);
+        sleep(5);
+    }
 
 }
