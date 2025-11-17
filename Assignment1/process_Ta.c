de583355b23af8b05d093769fdd84947ffb70a2f
Francesco <f.sisani2002@gmail.com>
2025-11-17 13:05:39 +0100

    Commit 2 17/11 - Fixed random generation - Fixed possible OOB generation

--- Assignment1/process_Ta.c
+++ Assignment1/process_Ta.c
@@ -8,6 +8,7 @@
 #include <sys/wait.h>
 #include <curses.h>
 #include <sys/time.h>
+#include <time.h>
 
 int window_width;
 int window_height;
@@ -63,6 +64,8 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    srand(time(NULL) + getpid());
+
     //convert the argument to an integer file descriptor
     int fdTa = atoi(argv[1]);
     char buffer[100];
@@ -70,9 +73,9 @@ int main(int argc, char *argv[])
 
     while(1){
         //write it to pipe
-       int x_coord = rand() %  window_width;
-       int y_coord = rand() %  window_height;
-
+        sleep(1);
+        int x_coord = rand() % (window_width - 10);
+        int y_coord = rand() % (window_height - 10);
         sprintf(buffer, "%d,%d", x_coord, y_coord);
         write(fdTa, buffer, strlen(buffer)+1);
         sleep(3);
